4bb6ce269a6291e12ba15cdecca0556cadeb8e12
Ruben Hernandez <hernandzruben9@gmail.com>
2024-03-14 20:30:39 -0400

    dwdd

--- src/main/java/ruben/eduardo/knn/HelloController.java
+++ src/main/java/ruben/eduardo/knn/HelloController.java
@@ -34,23 +34,59 @@ public class HelloController  {
     public void addRandomDataToChart() {
         // Datos de ejemplo, reemplaza esto con tus datos reales
         double[][] datosComprar = {
-                {4407489,65,1.20},
-                {1297060,75,1.50},
-                {1131490,70,0.85},
-                {2000000,62,1.00},
-
+                {4407489, 65, 1.20},
+                {1297060, 75, 1.50},
+                {1131490, 50, 0.85},
+                {2000000, 62, 1.00},
+                {3500000, 70, 1.25},
+                {2750000, 68, 1.10},
+                {3250000, 72, 1.40},
+                {1500000, 63, 1.05},
+                {1750000, 67, 1.15},
+                {2250000, 69, 1.30},
+                {3800000, 71, 1.35},
+                {4000000, 74, 1.45},
+                {4200000, 76, 1.55},
+                {4500000, 78, 1.65},
+                {4700000, 80, 1.75}
         };
 
         double[][] datosMantener = {
-                {2537623,60,-0.30},
-
+                {5537623, 60, -0.30},
+                {2137890, 60, 0.10},
+                {4500000, 55, -0.05},
+                {5600000, 58, 0.00},
+                {4400000, 57, -0.10},
+                {2300000, 59, 0.05},
+                {5200000, 56, -0.20},
+                {4100000, 54, -0.15},
+                {5700000, 53, 0.20},
+                {2800000, 52, 0.25},
+                {1900000, 51, 0.30},
+                {4100000, 50, 0.35},
+                {6100000, 49, 0.40},
+                {5200000, 48, 0.45},
+                {3300000, 47, 0.50}
         };
 
         double[][] datosVender = {
-                {1860866,55,-0.75},
-                {3000000,64,1.10},
-
+                {1860866, 30, -0.75},
+                {1260866, 20, -0.65},
+                {3000000, 25, 1.10},
+                {3100000, 35, -0.80},
+                {2200000, 40, -0.85},
+                {1300000, 45, -0.90},
+                {3400000, 50, -0.95},
+                {3500000, 55, -1.00},
+                {1600000, 28, -0.70},
+                {3700000, 22, -0.60},
+                {2800000, 18, -0.55},
+                {3900000, 16, -0.50},
+                {2000000, 14, -0.45},
+                {4100000, 12, -0.40},
+                {4200000, 10, -0.35}
         };
+
 
         // ... datos para "Vender" ...
 
@@ -60,7 +96,7 @@ public class HelloController  {
         for (double[] dato : datosComprar) {
             XYChart.Data<Number, Number> dataPoint = new XYChart.Data<>(dato[1], dato[2]);
             Circle circle = new Circle();
-            circle.setRadius(dato[0] * 0.000005); // Ajusta el factor de escala según sea necesario
+            circle.setRadius(dato[0] * 0.000002); // Ajusta el factor de escala según sea necesario
             circle.setFill(Color.GREEN);
             dataPoint.setNode(circle);
             series1.getData().add(dataPoint);
@@ -72,7 +108,7 @@ public class HelloController  {
         for (double[] dato : datosMantener) {
             XYChart.Data<Number, Number> dataPoint = new XYChart.Data<>(dato[1], dato[2]);
             Circle circle = new Circle();
-            circle.setRadius(dato[0] * 0.000005); // Ajusta el factor de escala según sea necesario
+            circle.setRadius(dato[0] * 0.000002); // Ajusta el factor de escala según sea necesario
             circle.setFill(Color.ORANGE);
             dataPoint.setNode(circle);
             series2.getData().add(dataPoint);
@@ -84,7 +120,7 @@ public class HelloController  {
         for (double[] dato : datosVender) {
             XYChart.Data<Number, Number> dataPoint = new XYChart.Data<>(dato[1], dato[2]);
             Circle circle = new Circle();
-            circle.setRadius(dato[0] * 0.000005); // Ajusta el factor de escala según sea necesario
+            circle.setRadius(dato[0] * 0.000002); // Ajusta el factor de escala según sea necesario
             circle.setFill(Color.RED);
             dataPoint.setNode(circle);
             series3.getData().add(dataPoint);
