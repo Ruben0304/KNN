ecbd134567cb78134c3873d5c4de48c85c8ff9ba
Ruben Hernandez <hernandzruben9@gmail.com>
2024-03-19 18:55:00 -0400

    dwdd

--- src/test/java/ruben/eduardo/knn/services/CargarDatosTest.java
+++ src/test/java/ruben/eduardo/knn/services/CargarDatosTest.java
@@ -1,25 +1,26 @@
 package ruben.eduardo.knn.services;
 
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
 import ruben.eduardo.knn.services.LectorFicheros;
 import ruben.eduardo.knn.interfaces.ILectorFicheros;
 
-import java.util.HashMap;
-import java.util.LinkedList;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CargarDatosTest {
 
-    private final ILectorFicheros c = new LectorFicheros();
+    private final LectorFicheros c = new LectorFicheros("D:/Proyectos/Java/Escuela/KNN/src/main/resources/ruben/eduardo/knn/Data/Fulbo.csv");
 
     //archivo indicadores
     @Test
     void leerEncabezado() {
 
-        c.leerEncabezado("D:/Proyectos/Java/Escuela/KNN/src/main/resources/ruben/eduardo/knn/Data/indicadores.csv").forEach(System.out::println);
-
+        c.leerEncabezado().forEach(System.out::println);
         // Importante comporbar que true es true , no quitar
         assertTrue(true);
 
@@ -28,16 +29,17 @@ class CargarDatosTest {
 
     @Test
     void leerArchivoClasificaciones() {
-        HashMap<LinkedList<Double>, String> f = c.leerArchivo("D:/Proyectos/Java/Escuela/KNN/src/main/resources/ruben/eduardo/knn/Data/indicadores.csv", 3);
+        HashMap<LinkedList<Double>, String> f = c.leerArchivo( 3);
         for (String s : f.values()) {
             System.out.println(s);
         }
+
         assertTrue(true);
     }
 
     @Test
     void leerArchivoValores() {
-        HashMap<LinkedList<Double>, String> f = c.leerArchivo("D:/Proyectos/Java/Escuela/KNN/src/main/resources/ruben/eduardo/knn/Data/indicadores.csv", 3);
+        HashMap<LinkedList<Double>, String> f = c.leerArchivo(3);
         for (LinkedList<Double> s : f.keySet()) {
             System.out.println(s);
         }
@@ -50,7 +52,7 @@ class CargarDatosTest {
     @Test
     void leerEncabezadoF() {
 
-        c.leerEncabezado("D:/Proyectos/Java/Escuela/KNN/src/main/resources/ruben/eduardo/knn/Data/Fulbo.csv").forEach(System.out::println);
+        c.leerEncabezado().forEach(System.out::println);
 
         // Importante comporbar que true es true , no quitar
         assertTrue(true);
@@ -59,7 +61,7 @@ class CargarDatosTest {
 
     @Test
     void leerArchivoClasificacionesF() {
-        HashMap<LinkedList<Double>, String> f = c.leerArchivo("D:/Proyectos/Java/Escuela/KNN/src/main/resources/ruben/eduardo/knn/Data/Fulbo.csv", 0);
+        HashMap<LinkedList<Double>, String> f = c.leerArchivo( 0);
         for (String s : f.values()) {
             System.out.println(s);
         }
@@ -68,10 +70,68 @@ class CargarDatosTest {
 
     @Test
     void leerArchivoValoresF() {
-        HashMap<LinkedList<Double>, String> f = c.leerArchivo("D:/Proyectos/Java/Escuela/KNN/src/main/resources/ruben/eduardo/knn/Data/Fulbo.csv", 0);
+        HashMap<LinkedList<Double>, String> f = c.leerArchivo( 0);
         for (LinkedList<Double> s : f.keySet()) {
             System.out.println(s);
         }
         assertTrue(true);
     }
+
+    @Test
+    void crearCsv1Millon() {
+        Faker faker = new Faker();
+        List<String> categorias = Arrays.asList("Electrónica", "Ropa", "Alimentos", "Libros", "Juguetes");
+        Random random = new Random();
+
+        try (FileWriter writer = new FileWriter("datos.csv")) {
+            // Escribir la fila de encabezado
+            writer.write("Numero1,Numero2,Numero3,Numero4,Categoria\n");
+
+            // Ciclo para generar los datos
+            for (int i = 1; i <= 1000000; i++) {
+                int num1 = faker.number().numberBetween(1, 100);
+                int num2 = faker.number().numberBetween(1, 100);
+                int num3 = faker.number().numberBetween(1, 100);
+                int num4 = faker.number().numberBetween(1, 100);
+                // Seleccionar una categoría aleatoria de la lista
+                String categoria = categorias.get(random.nextInt(categorias.size()));
+
+                writer.write(num1 + "," + num2 + "," + num3 + "," + num4 + "," + categoria + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assertTrue(true);
+    }
+
+    @Test
+    void obtenerPosicionClasificacion() {
+        assertEquals(0,c.obtenerPosicionClasificacion());
+    }
+
+    @Test
+    void crearCsv1MillonNC() {
+        Faker faker = new Faker();
+
+        try (FileWriter writer = new FileWriter("datosNC.csv")) {
+            // Escribir la fila de encabezado
+            writer.write("Numero1,Numero2,Numero3,Numero4\n");
+
+            // Ciclo para generar los datos
+            for (int i = 1; i <= 3000; i++) {
+                int num1 = faker.number().numberBetween(1, 100);
+                int num2 = faker.number().numberBetween(1, 100);
+                int num3 = faker.number().numberBetween(1, 100);
+                int num4 = faker.number().numberBetween(1, 100);
+
+
+                writer.write(num1 + "," + num2 + "," + num3 + "," + num4 + "," +"\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assertTrue(true);
+    }
+
+
 }
